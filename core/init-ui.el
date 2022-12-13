e108bdf1639f3ffb6aebe294bb3b52d57c78f8f0
roife <roifewu@gmail.com>
2022-12-14 03:52:13 +0800

    merge

--- core/init-ui.el
+++ core/init-ui.el
@@ -93,7 +93,6 @@
                             "\\\\" "://"))
   )
 
-
 ;; [tab-bar] Tab bar
 (use-package tab-bar
   :straight t
@@ -130,6 +129,33 @@
        'face (funcall tab-bar-tab-face-function tab))))
 
   (setq tab-bar-tab-name-format-function #'+tab-bar-tab-name-format)
+  )
+
+;; Highlight indentions
+(use-package highlight-indent-guides
+  :straight t
+  :hook ((prog-mode yaml-mode) . (lambda () (unless (> (car (buffer-line-statistics)) 3000) (highlight-indent-guides-mode 1))))
+  :init (setq highlight-indent-guides-method 'character
+              highlight-indent-guides-responsive 'top
+              highlight-indent-guides-suppress-auto-error t)
+  :config
+  ;; Don't display first level of indentation
+  (defun my-indent-guides-for-all-but-first-column (level responsive display)
+    (unless (< level 1)
+      (highlight-indent-guides--highlighter-default level responsive display)))
+  (setq highlight-indent-guides-highlighter-function
+        #'my-indent-guides-for-all-but-first-column)
+
+  ;; Disable in `macrostep' expanding
+  (with-eval-after-load 'macrostep
+    (advice-add #'macrostep-expand
+                :after (lambda (&rest _)
+                         (when highlight-indent-guides-mode
+                           (highlight-indent-guides-mode -1))))
+    (advice-add #'macrostep-collapse
+                :after (lambda (&rest _)
+                         (when (derived-mode-p 'prog-mode 'yaml-mode)
+                           (highlight-indent-guides-mode 1)))))
   )
 
 (provide 'init-ui)
