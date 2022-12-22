e712feaa7892a2bab5535b7bd9278bc4cb2423ea
roife <roifewu@gmail.com>
2022-12-23 05:08:13 +0800

    fix citre

--- core/init-prog.el
+++ core/init-prog.el
@@ -56,22 +56,27 @@
 (use-package citre
   :straight t
   :bind (:map prog-mode-map
-              ("C-c c j" . citre-jump+)
-              ("C-c c k" . citre-jump-back+)
+              ("C-c c j" . +citre-jump)
+              ("C-c c k" . +citre-jump-back)
               ("C-c c p" . citre-peek)
               ("C-c c a" . citre-ace-peek)
               ("C-c c u" . citre-update-this-tags-file))
   :init
   (require 'citre-config)
+  :config
+  (setq citre-auto-enable-citre-mode-modes '(prog-mode)
+        citre-default-create-tags-file-location 'global-cache
+        citre-use-project-root-when-creating-tags t
+        citre-prompt-language-for-ctags-command t)
 
-  (defun citre-jump+ ()
+  (defun +citre-jump ()
     "Jump to the definition of the symbol at point. Fallback to `xref-find-definitions'."
     (interactive)
     (condition-case _
         (citre-jump)
       (error (call-interactively #'xref-find-definitions))))
 
-  (defun citre-jump-back+ ()
+  (defun +citre-jump-back ()
     "Go back to the position before last `citre-jump'. Fallback to `xref-go-back'."
     (interactive)
     (condition-case _
@@ -79,12 +84,8 @@
       (error (if (fboundp #'xref-go-back)
                  (call-interactively #'xref-go-back)
                (call-interactively #'xref-pop-marker-stack)))))
-  :config
-  (setq citre-auto-enable-citre-mode-modes '(prog-mode)
-        citre-default-create-tags-file-location 'global-cache
-        citre-use-project-root-when-creating-tags t
-        citre-prompt-language-for-ctags-command t)
 
+  ;; Use Citre xref backend as a [fallback]
   (define-advice xref--create-fetcher (:around (fn &rest args) fallback)
     (let ((fetcher (apply fn args))
           (citre-fetcher
@@ -96,13 +97,20 @@
               (funcall fetcher))
             (funcall citre-fetcher)))))
 
-  (defalias #'my-eglot-citre-capf
+  ;; Integration with [eglot]
+  (defalias #'+eglot-citre-capf
     (cape-super-capf #'eglot-completion-at-point #'citre-completion-at-point))
 
   (add-hook 'eglot-managed-mode-hook
-            (defun my-toggle-citre-eglot-capf ()
-              (if (eglot-managed-p)
-                  (add-to-list 'completion-at-point-functions #'my-eglot-citre-capf))))
+            (lambda () (if (eglot-managed-p)
+                      (add-to-list 'completion-at-point-functions #'+eglot-citre-capf))))
+
+  (dolist (func '(find-function
+                  consult-imenu
+                  consult-ripgrep
+                  citre-jump))
+    (advice-add func :before (lambda (&rest r)
+                               (xref-push-marker-stack (point-marker)))))
   )
 
 
