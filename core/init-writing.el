16ffafc67e5397e120c0097297ca1566eb44b606
roife <roifewu@gmail.com>
2023-01-02 18:15:09 +0800

    add org-fragtog

--- core/init-writing.el
+++ core/init-writing.el
@@ -32,7 +32,17 @@
         markdown-nested-imenu-heading-index t
         markdown-fontify-code-blocks-natively t)
   (add-to-list 'markdown-code-lang-modes '("rust" . rustic-mode))
-  (add-to-list 'markdown-code-lang-modes '("verilog" . verilog-mode)))
+  (add-to-list 'markdown-code-lang-modes '("verilog" . verilog-mode))
+  (add-to-list 'markdown-code-lang-modes '("agda" . agda2-mode)))
+
+
+;; [org-fragtog] Preview and edit latex in md/org
+(use-package org-fragtog
+  :straight t
+  :hook (markdown-mode . org-fragtog-mode)
+  :config
+  (setq org-latex-create-formula-image-program 'dvisvgm)
+  (plist-put org-format-latex-options :scale 1.5))
 
 
 ;; [auctex]
