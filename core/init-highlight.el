e98331b3349d3ec2ac550b9aafc27b4cce883937
roife <roifewu@gmail.com>
2023-08-06 15:09:52 +0800

    use indent-bars

--- core/init-highlight.el
+++ core/init-highlight.el
@@ -145,44 +145,53 @@
 
 
 ;; [highlight-indent-guides] Highlight indentions
-(use-package highlight-indent-guides
-  :straight t
-  :functions (macrostep-collapse macrostep-expand)
-  :hook ((prog-mode yaml-mode) . (lambda () (unless (> (car (buffer-line-statistics)) 3000) (highlight-indent-guides-mode 1))))
+;; (use-package highlight-indent-guides
+;;   :straight t
+;;   :functions (macrostep-collapse macrostep-expand)
+;;   :hook ((prog-mode yaml-mode) . (lambda () (unless (> (car (buffer-line-statistics)) 3000) (highlight-indent-guides-mode 1))))
+;;   :config
+;;   (setq highlight-indent-guides-method 'character
+;;               highlight-indent-guides-responsive 'top
+;;               highlight-indent-guides-suppress-auto-error t
+;;               highlight-indent-guides-delay 0.3)
+;;
+;;   ;; Don't display first level of indentation
+;;   (defun +indent-guides-for-all-but-first-column (level responsive display)
+;;     (unless (< level 1)
+;;       (highlight-indent-guides--highlighter-default level responsive display)))
+;;   (setq highlight-indent-guides-highlighter-function
+;;         #'+indent-guides-for-all-but-first-column)
+;;
+;;   ;; Disable in `macrostep' expanding
+;;   (with-eval-after-load 'macrostep
+;;     (advice-add #'macrostep-expand
+;;                 :after (lambda (&rest _)
+;;                          (when highlight-indent-guides-mode
+;;                            (highlight-indent-guides-mode -1))))
+;;     (advice-add #'macrostep-collapse
+;;                 :after (lambda (&rest _)
+;;                          (when (derived-mode-p 'prog-mode 'yaml-mode)
+;;                            (highlight-indent-guides-mode 1)))))
+;;
+;;   ;; HACK: `highlight-indent-guides' calculates its faces from the current theme,
+;;   ;; but is unable to do so properly in terminal Emacs
+;;   (defun +highligh-indent-guides-auto-set-faces (&rest _)
+;;     (when (display-graphic-p)
+;;             (highlight-indent-guides-auto-set-faces)))
+;;   (if (daemonp)
+;;       (add-hook 'server-after-make-frame-hook
+;;                 #'+highligh-indent-guides-auto-set-faces)
+;;     (advice-add #'enable-theme :after #'+highligh-indent-guides-auto-set-faces))
+;;   )
+
+
+;; [indent-bars] Highlight indentions effectively
+(use-package indent-bars
+  :straight (indent-bars :type git :host github :repo "jdtsmith/indent-bars")
+  :hook (prog-mode . indent-bars-mode)
   :config
-  (setq highlight-indent-guides-method 'character
-              highlight-indent-guides-responsive 'top
-              highlight-indent-guides-suppress-auto-error t
-              highlight-indent-guides-delay 0.3)
-
-  ;; Don't display first level of indentation
-  (defun +indent-guides-for-all-but-first-column (level responsive display)
-    (unless (< level 1)
-      (highlight-indent-guides--highlighter-default level responsive display)))
-  (setq highlight-indent-guides-highlighter-function
-        #'+indent-guides-for-all-but-first-column)
-
-  ;; Disable in `macrostep' expanding
-  (with-eval-after-load 'macrostep
-    (advice-add #'macrostep-expand
-                :after (lambda (&rest _)
-                         (when highlight-indent-guides-mode
-                           (highlight-indent-guides-mode -1))))
-    (advice-add #'macrostep-collapse
-                :after (lambda (&rest _)
-                         (when (derived-mode-p 'prog-mode 'yaml-mode)
-                           (highlight-indent-guides-mode 1)))))
-
-  ;; HACK: `highlight-indent-guides' acalculates its faces from the current theme,
-  ;; but is unable to do so properly in terminal Emacs
-  (defun +highligh-indent-guides-auto-set-faces (&rest _)
-    (when (display-graphic-p)
-            (highlight-indent-guides-auto-set-faces)))
-  (if (daemonp)
-      (add-hook 'server-after-make-frame-hook
-                #'+highligh-indent-guides-auto-set-faces)
-    (advice-add #'enable-theme :after #'+highligh-indent-guides-auto-set-faces))
-  )
+  (setq indent-bars-display-on-blank-lines nil
+        indent-bars-width-frac 0.3))
 
 
 ;; [symbol-overlay] Highlight symbols
