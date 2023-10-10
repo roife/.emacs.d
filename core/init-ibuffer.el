1bb26f27ccb81b900853fae6e667597bc2a1d856
roife <roifewu@gmail.com>
2023-10-10 20:08:31 +0800

    change ret in ibuffer

--- core/init-ibuffer.el
+++ core/init-ibuffer.el
@@ -1,7 +1,9 @@
 ;;; -*- lexical-binding: t -*-
 
 (use-package ibuffer
-  :bind ("C-x C-b" . ibuffer)
+  :bind (("C-x C-b" . ibuffer)
+         :map ibuffer-mode-map
+         ("RET" . ibuffer-visit-buffer-other-window))
   :config
   (setq ibuffer-show-empty-filter-groups nil
         ibuffer-use-other-window t)
