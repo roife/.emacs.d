214c2de591f3f79ae60893476560d3e5cb1266b2
roife <roifewu@gmail.com>
2023-10-28 20:06:40 +0800

    clean ime

--- core/init-ime.el
+++ core/init-ime.el
@@ -26,8 +26,6 @@
   (rime-preedit-face ((t (:inherit mode-line-active :background nil
                                    :inverse-video nil :underline t))))
   :bind ("s-SPC" . toggle-input-method)
-  :init
-  (setq default-input-method "rime")
   :config
   (when (eq system-type 'darwin)
     (setq rime-librime-root "~/.emacs.d/librime/dist/"
@@ -35,7 +33,7 @@
 
   (setq rime-show-candidate 'posframe
         rime-show-preedit 'inline
-        rime-posframe-properties '(:border-width 7))
+        rime-posframe-properties '(:internal-border-width 7))
   )
 
 ;; `defmacro' cannot be placed in use-package
@@ -55,47 +53,27 @@
          (after-init . sis-global-inline-mode)
          ;; Enable the context-mode for all buffers
          (after-init . sis-global-context-mode)
-         ;; Auto refresh sis state
-         ;; (after-init . sis-auto-refresh-mode)
-         ;; Respect mode
-         (after-init . sis-global-respect-mode)
          ;; Colored cursor
          (after-init . sis-global-cursor-color-mode))
   :config
-  (setq sis-inline-tighten-head-rule nil
-        sis-prefix-override-keys (list "C-c" "C-x" "C-h" "C-,"))
+  ;; Use emacs-rime as default
+  (sis-ism-lazyman-config nil "rime" 'native)
 
   ;; HACK: Set cursor color automatically
   (add-hook! +theme-changed-hook :call-immediately
     (defun +sis-set-other-cursor-color ()
-      (setq sis-other-cursor-color (face-foreground 'success nil t))))
+      (setq sis-other-cursor-color (face-foreground 'error nil t))))
 
-  ;; Use emacs-rime as default
-  (sis-ism-lazyman-config nil "rime" 'native)
+  (defun +sis-remove-head-space-after-cc-punc (tighten-fore-to)
+    (when (memq (char-before) '(?， ?。 ?？ ?！ ?； ?：))
+      (delete-char 1)))
+  (setq sis-inline-tighten-head-rule #'+sis-remove-head-space-after-cc-punc)
 
   ;; Context mode
   (add-hook 'meow-leave-insert-mode-hook #'sis-set-english)
   (add-to-list 'sis-context-hooks 'meow-enter-insert-mode-hook)
 
-  ;; WORKAROUND: conflicts with keypad
-  (add-hook! meow-leave-insert-mode-hook #'sis-prefix-override-buffer-disable)
-  (add-hook! meow-enter-insert-mode-hook #'sis-prefix-override-buffer-enable)
-  (add-hook! (meow-normal-mode meow-motion-mode)
-    (defun +sis-meow-set-english ()
-      (when (or meow-normal-mode
-                meow-motion-mode)
-        (sis-set-english))))
-
-  ;; WORKAROUND: conflicts with kbd macro
-  (defadvice! +sis-disable-prefix-override (&rest _)
-    :before #'start-kbd-macro
-    (sis-prefix-override-buffer-disable))
-  (defadvice! +sis-enable-prefix-override (&rest _)
-    :before #'end-kbd-macro
-    (when (bound-and-true-p meow-insert-mode)
-      (sis-prefix-override-buffer-enable)))
-
-  ;; HACK: Ignore some mode with context mode
+  ;; Ignore some mode with context mode
   (defadvice! +sis-context-guess-ignore-modes (fn &rest args)
     :around #'sis--context-guess
     (if (derived-mode-p 'pdf-view-mode)
@@ -109,8 +87,7 @@
                    (and (derived-mode-p 'telega-chat-mode)
                         (or (and (= (point) telega-chatbuf--input-marker) ; beginning of input
                                  (eolp))
-                            (sis--context-other-p back-detect fore-detect))
-                        )))
+                            (sis--context-other-p back-detect fore-detect)))))
       'other))
 
   (add-to-list 'sis-context-detectors #'+sis-context-switching-other)
@@ -118,14 +95,15 @@
   ;; Inline-mode
   (defvar-local +sis-inline-english-last-space-pos nil
     "The last space position in inline mode.")
+
   (add-hook! sis-inline-english-deactivated-hook
     (defun +sis-line-set-last-space-pos ()
       (when (eq (char-before) ?\s)
-          (setq +sis-inline-english-last-space-pos (point)))))
+        (setq +sis-inline-english-last-space-pos (point)))))
 
   (add-hook! sis-inline-mode-hook
     (defun +sis-inline-add-post-self-insert-hook ()
-      (add-hook! (post-self-insert-hook) :local
+      (add-hook! post-self-insert-hook :local
         (defun +sis-inline-remove-redundant-space ()
           (when (and (eq +sis-inline-english-last-space-pos (1- (point)))
                      (looking-back " [，。？！；：]"))
