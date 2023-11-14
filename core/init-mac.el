26b11bf394c2310b02c0447fade8775c6fca2fe1
roife <roifewu@gmail.com>
2023-11-15 02:20:08 +0800

    add emt

--- core/init-mac.el
+++ core/init-mac.el
@@ -10,6 +10,10 @@
   :bind (("C-c d i" . osx-dictionary-search-input)
          ("C-c d d" . osx-dictionary-search-pointer)))
 
+(use-package emt
+  :straight (:host github :repo "roife/emt")
+  :hook (after-init . emt-mode))
+
 (add-hook! ns-system-appearance-change-functions
   (defun +mac-auto-change-theme-with-system (&rest _)
     (+load-theme)))
