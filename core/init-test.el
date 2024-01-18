6a58e7921854d14a834da7be006edd26b1b64498
roife <roifewu@gmail.com>
2024-01-18 19:01:46 +0800

    add test configs

--- core/init-test.el
+++ core/init-test.el
@@ -1,4 +1,20 @@
 ;;; -*- lexical-binding: t -*-
+(defun jdtls-command-contact (&optional interactive)
+  (let* ((jdtls-cache-dir (file-name-concat user-emacs-directory "cache" "lsp-cache"))
+         (project-dir (file-name-nondirectory (directory-file-name (project-root (project-current)))))
+         (data-dir (expand-file-name (file-name-concat jdtls-cache-dir (md5 project-dir))))
+         (jvm-args `("-Xmx8G"
+                      ;; "-XX:+UseG1GC"
+                      "-XX:+UseZGC"
+                      "-XX:+UseStringDeduplication"
+                      ;; "-XX:FreqInlineSize=325"
+                      ;; "-XX:MaxInlineLevel=9"
+                      "-XX:+UseCompressedOops"))
+         (jvm-args (mapcar (lambda (arg) (concat "--jvm-arg=" arg)) jvm-args))
+         ;; tell jdtls the data directory and jvm args
+         (contact (append '("jdtls") jvm-args `("-data" ,data-dir))))
+    contact))
+
 (with-eval-after-load "eglot"
   (add-to-list 'eglot-stay-out-of 'flymake)
 
@@ -16,7 +32,9 @@
                          (gethash (expand-file-name buffer-file-name)
                                   eglot--servers-by-xrefed-file)))))))
 
-  (push '(verilog-mode . ("vizsla")) eglot-server-programs) 
+  (push '(verilog-mode . ("vizsla")) eglot-server-programs)
+  (push '(java-mode . jdtls-command-contact) eglot-server-programs)
+  (setq eglot-workspace-configuration '(:rust-analyzer (:completion (:limit 10))))
   )
 
 (setq eglot-x-enable-snippet-text-edit nil)
