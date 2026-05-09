2a82b87f85df1caba4bac44230be3c74bd964470
roife <roifewu@gmail.com>
2026-05-09 21:40:29 +0800

    add arg thing

--- core/init-modal.el
+++ core/init-modal.el
@@ -1,5 +1,73 @@
 ;;; -*- lexical-binding: t; -*-
 
+(require 'subr-x)
+
+(defun +thing--arg-parent-bounds-near-point ()
+  "Return surrounding list-like bounds around point or nearby whitespace."
+  (or (+thing--arg-parent-bounds)
+      (save-excursion
+        (skip-chars-forward " \t\n\r")
+        (+thing--arg-parent-bounds))
+      (save-excursion
+        (skip-chars-backward " \t\n\r")
+        (+thing--arg-parent-bounds))))
+
+(defun +thing--arg-parent-bounds ()
+  "Return bounds of the nearest surrounding list-like form."
+  (save-excursion
+    (let ((pt (point)))
+      (when-let* ((open (nth 1 (syntax-ppss
+                                (cond
+                                 ((memq (char-after pt) '(?\) ?\] ?\}))
+                                  (max (point-min) (1- pt)))
+                                 ((memq (char-after pt) '(?\( ?\[ ?\{))
+                                  (min (point-max) (1+ pt)))
+                                 (t pt))))))
+        (cons open (ignore-errors (scan-sexps open 1)))))))
+
+(defun +bounds-of-thing-at-point-arg (&optional bounds)
+  "Return bounds of the comma-separated argument at point.
+
+When BOUNDS is non-nil, include the adjacent comma separator when
+possible.  For non-final arguments the trailing separator is
+included; for the final argument the leading separator is included."
+  (when-let* ((pt (point))
+              (parent (+thing--arg-parent-bounds-near-point))
+              (open (car parent))
+              (close (cdr parent)))
+    (save-excursion
+      (let (prev next)
+        (goto-char (1+ open))
+        (while (and (< (point) close) (not next))
+          (when (and (eq (char-after) ?,)
+                     (eq (nth 1 (syntax-ppss (point))) open))
+            (if (< (point) pt)
+                (setq prev (point))
+              (setq next (point))))
+          (condition-case nil
+              (forward-sexp 1)
+            (error (forward-char 1))))
+        (let ((beg (if prev (1+ prev) (1+ open)))
+              (end (if next next (1- close))))
+          (goto-char beg)
+          (skip-chars-forward " \t\n\r" end)
+          (setq beg (point))
+          (goto-char end)
+          (skip-chars-backward " \t\n\r" beg)
+          (let ((trimmed-end (point)))
+            (when (< beg trimmed-end)
+              (if (not bounds)
+                  (cons beg trimmed-end)
+                (cond
+                 (next
+                  (goto-char (1+ next))
+                  (skip-chars-forward " \t\n\r" (1- close))
+                  (cons beg (point)))
+                 (prev
+                  (cons prev trimmed-end))
+                 (t
+                  (cons beg trimmed-end)))))))))))
+
 ;; [meow] Modal editing
 (use-package meow
   :straight t
@@ -17,6 +85,10 @@
                                                (keypad . "K")
                                                (insert . "I")
                                                (beacon . "B")))
+  (add-to-list 'meow-char-thing-table '(?a . arg))
+  (meow-thing-register 'arg
+                       #'+bounds-of-thing-at-point-arg
+                       (lambda () (+bounds-of-thing-at-point-arg t)))
 
   ;; [motion]
   (meow-motion-overwrite-define-key
